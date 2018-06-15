9aa5d253fc034fac5fedddfbc8d0dc1019b2c3da
ivanaradi <39125701+ivanaradi@users.noreply.github.com>
2018-06-15 11:18:17 +0200

    dodat converter za grupu i izmenjena registracija obavestenja

--- Startapi/src/java/controler/GrupaConverter.java
+++ Startapi/src/java/controler/GrupaConverter.java
@@ -5,38 +5,64 @@
  */
 package controler;
 
+import static com.sun.faces.util.MessageUtils.CONVERSION_ERROR_MESSAGE_ID;
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 import model.Grupa;
-import model.GrupaDAO;
 
 /**
  *
  * @author Korisnik
  */
 @FacesConverter(value = "grupaconverter")
-public class GrupaConverter implements Converter{
+public class GrupaConverter implements Converter {
 
-     @Override
-    public Object getAsObject(FacesContext context, UIComponent component, String newValue) {
-        Grupa grupa = GrupaDAO.dohvatiGrupuPoID(Integer.parseInt(newValue));
-       
+//     @Override
+//    public Object getAsObject(FacesContext context, UIComponent component, String newValue) {
+//        Grupa grupa = GrupaDAO.dohvatiGrupuPoID(Integer.parseInt(newValue));
+//       
+//        return grupa;
+//    }
+//
+//    @Override
+//    public String getAsString(FacesContext context, UIComponent component, Object value) {
+//        String val = null;
+//       
+//            Grupa gr = (Grupa) value;
+//            val = Integer.toString(gr.getId());
+//     
+//        return val;
+//    }
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        String[] param = value.split(";");
+        Grupa grupa = new Grupa(Integer.parseInt(param[0]), param[1], param[2]);
+
         return grupa;
     }
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
         String val = null;
-       
+        if (value == null) {
+            return null;
+        }
+
+        try {
             Grupa gr = (Grupa) value;
-            val = Integer.toString(gr.getId());
-     
+            val = "" + gr.getId() + ";" + gr.getNaziv() + ";" + gr.getOpis();
+
+        } catch (ClassCastException ce) {
+            FacesMessage errMsg = new FacesMessage(CONVERSION_ERROR_MESSAGE_ID);
+            FacesContext.getCurrentInstance().addMessage(null, errMsg);
+            throw new ConverterException(errMsg.getSummary());
+        }
+
         return val;
     }
-    
-    
-    
-    
+
 }
